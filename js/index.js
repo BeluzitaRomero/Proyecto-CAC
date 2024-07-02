72e95296246deecd9a7cd6b06e4b202274e7b6d5
Belen Romero <romero.abelen@gmail.com>
2024-07-02 16:13:01 -0300

    agregado de api php en index.js y formPelicula.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,36 @@
-const API_KEY = "7271771e6043ab63ad877def2bc41391";
-const BASE_URL = "https://api.themoviedb.org/3";
-const API_URL = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`;
+//nuestra api local - fijense de alojarlo igual en htdocs -
+const API_URL = "http://localhost/backend-movies/getPeliculas.php";
 
-// Función para obtener las películas populares
-async function getPopularMovies() {
+// Pedimos películas desde API PHP
+async function getPeliculas() {
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    displayMovies(data.results);
+    displayMovies(data); // Llama a la función para mostrar las películas en el DOM
   } catch (error) {
     console.error("Error al obtener las películas:", error);
   }
 }
 
-// Función para mostrar las películas en el DOM
+// Funcion para mostrar las card de pelis
 function displayMovies(movies) {
-  console.log(movies, "MOVIES");
   const moviesContainer = document.getElementById("peliculas-container");
 
   movies.forEach((movie) => {
     const movieItem = document.createElement("article");
     movieItem.classList.add("card");
 
+    const title = document.createElement("h2");
+    title.innerText = movie.titulo;
+    movieItem.appendChild(title);
+
     const movieDetailLink = document.createElement("a");
     movieDetailLink.classList.add("text-decoration-none", "text-dark");
     movieDetailLink.href = `pages/detalle.html?id=${movie.id}`;
 
     const movieImg = document.createElement("img");
-    movieImg.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    movieImg.alt = movie.title;
+    movieImg.src = movie.img;
+    movieImg.alt = movie.titulo;
     movieImg.classList.add("card-img-top");
 
     movieDetailLink.appendChild(movieImg);
@@ -37,6 +39,5 @@ function displayMovies(movies) {
     moviesContainer.appendChild(movieItem);
   });
 }
-
-//Llamada a la función para obtener y mostrar las películas
-getPopularMovies();
+//Invocola función para obtener y mostrar las películas
+getPeliculas();
