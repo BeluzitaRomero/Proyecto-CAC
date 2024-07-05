52b1ff812c5506eb57ec245074e03693ad7a8b2d
Belen Romero <romero.abelen@gmail.com>
2024-07-05 17:35:05 -0300

    session.js

--- js/login.js
+++ js/login.js
@@ -17,12 +17,26 @@ document.addEventListener("DOMContentLoaded", () => {
       if (response.ok) {
         const data = await response.json();
         console.log("Respuesta del servidor:", data);
+
+        // Guardar datos del usuario en localStorage
+        localStorage.setItem("user", JSON.stringify(data));
+
         alert("Login exitoso");
+
+        // Redirigir o mostrar contenido según el rol
+        if (data.rol === "admin") {
+          window.location.href = "../index.html"; // Redirigir a la página de administración
+        } else {
+          window.location.href = "../index.html"; // Redirigir a la página de usuario
+        }
       } else {
-        console.error("Error en la solicitud:", response.statusText);
+        const errorData = await response.json();
+        console.error("Error en la solicitud:", errorData.mensaje);
+        alert(`Error: ${errorData.mensaje}`);
       }
     } catch (error) {
       console.error("Error en la solicitud:", error);
+      alert("Ocurrió un error al iniciar sesión. Inténtalo nuevamente.");
     }
 
     form.reset();
