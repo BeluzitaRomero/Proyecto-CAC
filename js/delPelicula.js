795e1463022be9f04812b39cde0d689bc0173c26
martinebalbi <168096336+martinebalbi@users.noreply.github.com>
2024-07-11 00:28:00 -0300

    modificaciones por admin

---
+++ js/delPelicula.js
@@ -0,0 +1,32 @@
+function eliminarPelicula(idPelicula) {
+
+  const confirmacion = confirm(`¿Estás seguro de que deseas eliminar la película`);
+
+  console.log(idPelicula);
+  if (confirmacion) {
+    fetch('https://proyecto-movies-cac.000webhostapp.com/crud/delPelicula.php', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: idPelicula
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        alert(data.message || data.error);
+        window.location.reload();
+      })
+      
+      .catch(error => console.error(error));
+  } else {
+    alert('La película no se eliminó.');
+  }
+
+
+}
+
+
+
